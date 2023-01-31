fca56bd823476b1017fe41c93c6cb7611c19fba8
ChrisChafin <chrischafin11@gmail.com>
2023-01-31 17:35:47 -0600

    Added transistions
    Fix gradients
    Cleaned up CSS
    Fixed JS issues
    Simplified class names

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
 $(document).ready(() => {
+    $('.content-body').each(function(){
+        if($(this).hasClass('about')){
+            $(this).addClass('open');
+        } else {
+            $(this).addClass('hidden');
+        }
+    });
     // if you click skill toggle li
     $('.subject-toggle ul.skills-toggle li').on('click', function(){
         let activeId = $(this).attr('id');
-        // for each content element
-        $('.content').each(function(){
-            //if it has the same class as the active id
-            if($(this).hasClass(activeId)){
-                //show it
-                if ($(this).hasClass('show') && !$(this).hasClass("about")){
-                    $(this).parent().find('.about').toggleClass('show hide');
-                    $(this).toggleClass('show hide');
-                }
-                else if ($(this).hasClass('show') && $(this).hasClass("about")){
-                    return;
-                }
-                else {
-                    $(this).toggleClass('show hide');
+        $('.content-body').each(function(){
+            if(!$(this).hasClass(activeId)){
+                if($(this).hasClass('open')){
+                    $(this).removeClass('open').addClass('hidden');
                 }
             } else {
-                if($(this).hasClass('show')){
-                    $(this).toggleClass('show hide');
-                }
+                $(this).addClass('open').removeClass('hidden');
             }
         });
     });
